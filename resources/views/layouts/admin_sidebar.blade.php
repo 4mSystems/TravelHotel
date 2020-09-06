14b1e038f28f58c40e7ba9da0fd9da11b9ab6965
mohamednasser2019 <mimonasser50@gmail.com>
2020-09-06 11:19:26 +0200

    sd

---
+++ resources/views/layouts/admin_sidebar.blade.php
@@ -0,0 +1,30 @@
+<!-- main menu-->
+<div data-scroll-to-active="true" class="main-menu menu-fixed menu-dark menu-accordion menu-shadow">
+
+    <!-- main menu content-->
+    <div class="main-menu-content">
+        <ul id="main-menu-navigation" data-menu="menu-navigation" class="navigation navigation-main">
+
+            <li class=" nav-item">
+                <a href="{{url('home')}}"><i class="icon-home3"></i>
+                    <span data-i18n="nav.dash.main"
+                          class="menu-title">{{trans('admin.nav_home')}}</span></a>
+
+            </li>
+
+                <li class=" nav-item">
+                    <a href="{{url('super_Admin')}}"><i class="icon-users"></i>
+                        <span data-i18n="nav.dash.main"
+                              class="menu-title">{{trans('admin.nav_add_Super_Admin')}}</span></a>
+
+                </li>
+
+
+        </ul>
+
+        {{--        </ul>--}}
+    </div>
+    <!-- /main menu content-->
+
+</div>
+<!-- / main menu-->
